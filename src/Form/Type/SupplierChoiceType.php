5dbe20f61d989e43492b1c8fac2554db9e9831eb
Michael Babker <mbabker@flbab.com>
2017-02-14 10:31:22 -0600

    Add choice type form

---
+++ src/Form/Type/SupplierChoiceType.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * This file is part of the BabDevSyliusSupplierBundle package.
+ *
+ * (c) Michael Babker
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BabDev\SyliusSupplierBundle\Form\Type;
+
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Symfony\Bridge\Doctrine\Form\DataTransformer\CollectionToArrayTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class SupplierChoiceType extends AbstractType
+{
+    /**
+     * @var RepositoryInterface
+     */
+    private $supplierRepository;
+
+    /**
+     * @param RepositoryInterface $supplierRepository
+     */
+    public function __construct(RepositoryInterface $supplierRepository)
+    {
+        $this->supplierRepository = $supplierRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        if ($options['multiple']) {
+            $builder->addModelTransformer(new CollectionToArrayTransformer());
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'choices' => function (Options $options) {
+                return $this->supplierRepository->findAll();
+            },
+            'choice_value' => 'code',
+            'choice_label' => 'name',
+            'choice_translation_domain' => false,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return ChoiceType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'babdev_sylius_supplier_choice';
+    }
+}
