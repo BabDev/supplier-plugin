c9c08d1d79cee314c108212891217550c1e7baca
Michael Babker <michael.babker@gmail.com>
2018-04-18 15:27:11 -0500

    Catch up plugin to skeleton structure

--- tests/Application/app/AppKernel.php
+++ tests/Application/app/AppKernel.php
@@ -1,46 +1,32 @@
 <?php
 
-/*
- * This file is part of the BabDevSupplierPlugin package.
- *
- * (c) Michael Babker
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+declare(strict_types=1);
 
 use Sylius\Bundle\CoreBundle\Application\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-/**
- * @author Michael Babker <michael.babker@gmail.com>
- */
 final class AppKernel extends Kernel
 {
     /**
      * {@inheritdoc}
      */
-    public function registerBundles()
+    public function registerBundles(): array
     {
-        return array_merge(
-            [
-                new BabDev\SupplierPlugin\BabDevSupplierPlugin(),
-            ],
-            parent::registerBundles(),
-            [
-                new Sylius\Bundle\AdminBundle\SyliusAdminBundle(),
-                new Sylius\Bundle\ShopBundle\SyliusShopBundle(),
+        return array_merge(parent::registerBundles(), [
+            new \Sylius\Bundle\AdminBundle\SyliusAdminBundle(),
+            new \Sylius\Bundle\ShopBundle\SyliusShopBundle(),
 
-                new FOS\OAuthServerBundle\FOSOAuthServerBundle(), // Required by SyliusAdminApiBundle
-                new Sylius\Bundle\AdminApiBundle\SyliusAdminApiBundle(),
-            ]
-        );
+            new \FOS\OAuthServerBundle\FOSOAuthServerBundle(), // Required by SyliusApiBundle
+            new \Sylius\Bundle\AdminApiBundle\SyliusAdminApiBundle(),
+
+            new \BabDev\SupplierPlugin\BabDevSupplierPlugin(),
+        ]);
     }
 
     /**
      * {@inheritdoc}
      */
-    public function registerContainerConfiguration(LoaderInterface $loader)
+    public function registerContainerConfiguration(LoaderInterface $loader): void
     {
         $loader->load($this->getRootDir() . '/config/config.yml');
     }
