76580c93396cfaa2ed82127332870b5fad5363f1
Michael Babker <michael.babker@gmail.com>
2021-03-11 18:01:30 -0600

    Simplify plugin class

--- src/BabDevSyliusSupplierPlugin.php
+++ src/BabDevSyliusSupplierPlugin.php
@@ -13,68 +13,26 @@ declare(strict_types=1);
 
 namespace BabDev\SyliusSupplierPlugin;
 
+use BabDev\SyliusSupplierPlugin\DependencyInjection\BabDevSyliusSupplierExtension;
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
-use Sylius\Bundle\ResourceBundle\AbstractResourceBundle;
-use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class BabDevSyliusSupplierPlugin extends AbstractResourceBundle
+final class BabDevSyliusSupplierPlugin extends Bundle
 {
     use SyliusPluginTrait;
 
-    /**
-     * {@inheritdoc}
-     */
     protected function getBundlePrefix(): string
     {
         return 'babdev_sylius_supplier';
     }
 
-    /**
-     * @psalm-suppress DocblockTypeContradiction
-     * @psalm-suppress InvalidReturnStatement
-     * @psalm-suppress InvalidReturnType
-     */
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if (null === $this->containerExtension) {
-            $extension = $this->createContainerExtension();
-
-            if (null !== $extension) {
-                /**
-                 * @phpstan-ignore-next-line
-                 */
-                if (!$extension instanceof ExtensionInterface) {
-                    throw new \LogicException(sprintf('Extension %s must implement %s.', get_class($extension), ExtensionInterface::class));
-                }
-
-                $this->containerExtension = $extension;
-            } else {
-                $this->containerExtension = false;
-            }
+            $this->containerExtension = new BabDevSyliusSupplierExtension();
         }
 
-        /**
-         * @phpstan-ignore-next-line
-         */
-        return $this->containerExtension === false ? null : $this->containerExtension;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getSupportedDrivers(): array
-    {
-        return [
-            SyliusResourceBundle::DRIVER_DOCTRINE_ORM,
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getModelNamespace(): ?string
-    {
-        return __NAMESPACE__ . '\\Model';
+        return $this->containerExtension ?? null;
     }
 }
